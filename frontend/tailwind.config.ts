9cadbdb9a28eb895d4226d13194bd3a766540587
kamegoro <tentyo0620@icloud.com>
2023-09-18 15:18:37 +0900

    add: environment modules

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,53 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Montserrat', '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial', 'sans-serif'],
+    },
+    fontSize: {
+      xs: '0.75rem',
+      sm: '14px',
+      base: '16px',
+      lg: '20px',
+    },
+    fontWeight: {
+      regular: 400,
+      medium: 600,
+      bold: 700,
+      xbold: 800,
+    },
+    lineHeight: {
+      xs: '1rem',
+      sm: '16px',
+      base: '20px',
+      lg: '24px',
+    },
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      blue: '#1947E5',
+      pink: '#FF89BB',
+      yellow: '#FFBD12',
+      green: '#00C6AE',
+      red: '#F95A2C',
+      text: {
+        primary: '#18191F',
+        secondary: '#474A57',
+      },
+    },
+    boxShadow: {
+      sm: '0 3px black',
+      DEFAULT: '0 8px black',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        '3xl': '3rem',
       },
     },
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
-export default config
+};
